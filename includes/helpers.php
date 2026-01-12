71115a8e8e7a397274cc146e55e5521756968324
TenzyBravo <temwanizangizgambo@gmail.com>
2026-01-13 00:51:05 +0200

    Make currency display generic: add CURRENCY_CODE/NAME and rename formatKwacha to formatCurrency (with backwards wrapper)

--- includes/helpers.php
+++ includes/helpers.php
@@ -1,6 +1,11 @@
 <?php
 require_once __DIR__ . '/../config/constants.php';
 
-function formatKwacha(float $amount): string {
+function formatCurrency(float $amount): string {
     return AppConfig::CURRENCY_SYMBOL . ' ' . number_format($amount, 2);
+}
+
+// Backwards-compatible wrapper for older calls
+function formatKwacha(float $amount): string {
+    return formatCurrency($amount);
 }
